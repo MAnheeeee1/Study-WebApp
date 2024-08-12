ec25af54f2a41c1f4f3d2ae459237813134746da
Manh.D <07mandao@skola.boras.se>
2024-08-12 21:09:18 +0200

    Switched back to vsCode from webstorm, added a function to add more flashcards

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
 let userFlashCards = {};
 let flashCardDialog = document.getElementById("flashCardDialog");
 let createFcBtn = document.getElementById("createFlashCard");
-
-createFcBtn.addEventListener("click", ()=>{
-    flashCardDialog.show();
+let addFlashCardBtn = document.getElementById("addCard");
+let termsFieldset = document.getElementById("termQuestionField");
+let answerFieldset = document.getElementById("answerDefinitionField");
+createFcBtn.addEventListener("click", () => {
+  flashCardDialog.show();
 });
+
+addFlashCardBtn.addEventListener("click", addFlashCard);
+
+function addFlashCard() {
+  const termsFieldsetLabels = termsFieldset.querySelectorAll("label");
+  const flashCardNumber = termsFieldsetLabels.length + 1;
+
+  let newLabelforTerm = document.createElement("label");
+  newLabelforTerm.setAttribute("for", "term" + flashCardNumber);
+  let newInputForTerm = document.createElement("input");
+  newInputForTerm.setAttribute("type", "text");
+  newInputForTerm.setAttribute("id", "term" + flashCardNumber);
+
+  termsFieldset.appendChild(newLabelforTerm);
+  newLabelforTerm.appendChild(newInputForTerm);
+
+  let newLabelforAnswer = document.createElement("label");
+  newLabelforAnswer.setAttribute("for", "answer" + flashCardNumber);
+  let newInputForAnswer = document.createElement("input");
+  newInputForAnswer.setAttribute("type", "text");
+  newInputForAnswer.setAttribute("id", "answer" + flashCardNumber);
+
+  answerFieldset.appendChild(newLabelforAnswer);
+  newLabelforAnswer.appendChild(newInputForAnswer);
+}
