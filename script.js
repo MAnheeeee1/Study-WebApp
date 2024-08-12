f3aa9659a2a4b11e6baac5494db32b1b11299fae
Manh Dao <131762085+MAnheeeee1@users.noreply.github.com>
2024-08-12 20:34:05 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+let userFlashCards = {};
+let flashCardDialog = document.getElementById("flashCardDialog");
+let createFcBtn = document.getElementById("createFlashCard");
+
+createFcBtn.addEventListener("click", ()=>{
+    flashCardDialog.show();
+});
