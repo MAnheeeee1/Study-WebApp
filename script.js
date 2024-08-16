5fad378f8eb1abd3202ee62574dd315f418a0d2c
Manh.D <07mandao@skola.boras.se>
2024-08-16 14:29:13 +0200

    Done with the function to add flashcard pairs to the userFlashCards object. Next session working on the display of these flash card pairs and reset the form in the dialog

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ let createFcBtn = document.getElementById("createFlashCard");
 let addFlashCardBtn = document.getElementById("addCard");
 let termsFieldset = document.getElementById("termQuestionField");
 let answerFieldset = document.getElementById("answerDefinitionField");
+let saveFlashCardBtn = document.getElementById("creataFlashCard");
+
 createFcBtn.addEventListener("click", () => {
   flashCardDialog.show();
 });
 
 addFlashCardBtn.addEventListener("click", addFlashCard);
+saveFlashCardBtn.addEventListener("click", saveFlashCard);
 
 function addFlashCard() {
   const termsFieldsetLabels = termsFieldset.querySelectorAll("label");
@@ -32,3 +35,28 @@ function addFlashCard() {
   answerFieldset.appendChild(newLabelforAnswer);
   newLabelforAnswer.appendChild(newInputForAnswer);
 }
+
+function saveFlashCard() {
+  let terms = termsFieldset.querySelectorAll("label");
+  let answers = answerFieldset.querySelectorAll("label");
+  let title = flashCardDialog.querySelector("#title").value;
+  let description = flashCardDialog.querySelector("#description").value;
+  let termsArray = [];
+  let answersArray = [];
+
+  for (term of terms) {
+    termsArray.push(term.querySelector("input").value);
+  }
+  for (answer of answers) {
+    answersArray.push(answer.querySelector("input").value);
+  }
+
+  let flashcard = {
+    description: description,
+    termsArray,
+    answers: answersArray,
+  };
+  userFlashCards[title] = flashcard;
+}
+
+function resetDialog() {}
