a55a5df547f9f1f1274b6e44522dff2c5609fa88
Manh.D <07mandao@skola.boras.se>
2024-08-18 19:07:59 +0200

    Har lagt till form validation ocurrently working on how to display the flashcards. Plan on having like small card and once user click on it shows the entire card. But havent figure out yet how to work with objects.

--- script.js
+++ script.js
@@ -5,35 +5,36 @@ let addFlashCardBtn = document.getElementById("addCard");
 let termsFieldset = document.getElementById("termQuestionField");
 let answerFieldset = document.getElementById("answerDefinitionField");
 let saveFlashCardBtn = document.getElementById("creataFlashCard");
+let flashCardContainer = document.getElementById("flashcardHolder");
 
 createFcBtn.addEventListener("click", () => {
   flashCardDialog.show();
 });
 
 addFlashCardBtn.addEventListener("click", addFlashCard);
-saveFlashCardBtn.addEventListener("click", saveFlashCard);
+saveFlashCardBtn.addEventListener("click", formValidation);
 
 function addFlashCard() {
   const termsFieldsetLabels = termsFieldset.querySelectorAll("label");
   const flashCardNumber = termsFieldsetLabels.length + 1;
 
-  let newLabelforTerm = document.createElement("label");
-  newLabelforTerm.setAttribute("for", "term" + flashCardNumber);
+  let newLabelForTerm = document.createElement("label");
+  newLabelForTerm.setAttribute("for", "term" + flashCardNumber);
   let newInputForTerm = document.createElement("input");
   newInputForTerm.setAttribute("type", "text");
   newInputForTerm.setAttribute("id", "term" + flashCardNumber);
 
-  termsFieldset.appendChild(newLabelforTerm);
-  newLabelforTerm.appendChild(newInputForTerm);
+  termsFieldset.appendChild(newLabelForTerm);
+  newLabelForTerm.appendChild(newInputForTerm);
 
-  let newLabelforAnswer = document.createElement("label");
-  newLabelforAnswer.setAttribute("for", "answer" + flashCardNumber);
+  let newLabelForAnswer = document.createElement("label");
+  newLabelForAnswer.setAttribute("for", "answer" + flashCardNumber);
   let newInputForAnswer = document.createElement("input");
   newInputForAnswer.setAttribute("type", "text");
   newInputForAnswer.setAttribute("id", "answer" + flashCardNumber);
 
-  answerFieldset.appendChild(newLabelforAnswer);
-  newLabelforAnswer.appendChild(newInputForAnswer);
+  answerFieldset.appendChild(newLabelForAnswer);
+  newLabelForAnswer.appendChild(newInputForAnswer);
 }
 
 function saveFlashCard() {
@@ -53,10 +54,28 @@ function saveFlashCard() {
 
   let flashcard = {
     description: description,
-    termsArray,
+    terms: termsArray,
     answers: answersArray,
   };
   userFlashCards[title] = flashcard;
+  flashCardDialog.close();
+  updateFlashCardContainer();
+  alert("Successfully saved your flashcards!");
 }
 
-function resetDialog() {}
+function formValidation(event) {
+  event.preventDefault();
+  const titleInput = document.querySelector("#title");
+  if (titleInput.value == null || titleInput.value == "") {
+    alert("Please input a title for your flashcard set");
+  } else {
+    saveFlashCard();
+  }
+}
+
+function updateFlashCardContainer() {
+  for (flashcard in userFlashCards) {
+    let flashCardTitle = userFlashCards[flashcard];
+    console.log(flashCardTitle);
+  }
+}
